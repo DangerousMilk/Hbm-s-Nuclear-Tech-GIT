f19182ddb859beb9d217ee2bf7cd41573d9e4132
DangerousMilk <galikgrisha@gmail.com>
2025-10-19 16:49:24 +0200

    Moved railings into their own class. Stairs now render with 2 textures.

--- src/main/java/com/hbm/render/block/RenderRailing.java
+++ src/main/java/com/hbm/render/block/RenderRailing.java
@@ -1,14 +1,17 @@
 package com.hbm.render.block;
 
+import com.hbm.blocks.generic.RailingBlock;
 import com.hbm.main.ResourceManager;
 import com.hbm.render.util.ObjUtil;
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.client.model.IModelCustom;
+import net.minecraftforge.client.model.obj.GroupObject;
 import net.minecraftforge.client.model.obj.WavefrontObject;
 import org.lwjgl.opengl.GL11;
 
@@ -24,37 +27,39 @@ public class RenderRailing implements ISimpleBlockRenderingHandler {
 
 	@Override
 	public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
-
 		GL11.glPushMatrix();
 		Tessellator tessellator = Tessellator.instance;
-		IIcon iicon = block.getIcon(0, metadata);
-		tessellator.setColorOpaque_F(1, 1, 1);
 
-		if(renderer.hasOverrideBlockTexture()) {
-			iicon = renderer.overrideBlockTexture;
-		}
+		IIcon steelIcon = block.getIcon(0, 0);
+		IIcon grateIcon = block.getIcon(1, 0);
+		tessellator.setColorOpaque_F(1, 1, 1);
 
 		GL11.glTranslated(0, -0.5D, 0);
 		GL11.glScaled(1D, 1D, 1D);
 		tessellator.startDrawingQuads();
-		ObjUtil.renderWithIcon((WavefrontObject) model, iicon, tessellator, 0, false);
-
+		if(this.renderID == RailingBlock.renderIDStairs) {
+			ObjUtil.renderPartWithIcon((WavefrontObject) model, "RailingStairs", steelIcon, tessellator, 0, false);
+			ObjUtil.renderPartWithIcon((WavefrontObject) model, "RailingSteps", grateIcon, tessellator, 0, false);
+		} else {
+			ObjUtil.renderWithIcon((WavefrontObject) model, steelIcon, tessellator, 0, false);
+		}
 		tessellator.draw();
-
 		GL11.glPopMatrix();
 	}
 
 	@Override
 	public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
-
 		Tessellator tessellator = Tessellator.instance;
 		int meta = world.getBlockMetadata(x, y, z);
-		IIcon iicon = block.getIcon(0, meta & 3);
+		IIcon steelIcon = block.getIcon(0, 0);
+		IIcon grateIcon = block.getIcon(1, 0);
 		tessellator.setColorOpaque_F(1, 1, 1);
 
+		/*
 		if(renderer.hasOverrideBlockTexture()) {
-			iicon = renderer.overrideBlockTexture;
+			steel = renderer.overrideBlockTexture;
 		}
+		 */
 
 		tessellator.setBrightness(block.getMixedBrightnessForBlock(world, x, y, z));
 		tessellator.setColorOpaque_F(1, 1, 1);
@@ -74,7 +79,12 @@ public class RenderRailing implements ISimpleBlockRenderingHandler {
 		}
 
 		tessellator.addTranslation(x + 0.5F, y, z + 0.5F);
-		ObjUtil.renderWithIcon((WavefrontObject) model, iicon, tessellator, rotation, true);
+		if(this.renderID == RailingBlock.renderIDStairs) {
+			ObjUtil.renderPartWithIcon((WavefrontObject) model, "RailingStairs", steelIcon, tessellator, rotation, true);
+			ObjUtil.renderPartWithIcon((WavefrontObject) model, "RailingSteps", grateIcon, tessellator, rotation, true);
+		} else {
+			ObjUtil.renderWithIcon((WavefrontObject) model, steelIcon, tessellator, rotation, true);
+		}
 		tessellator.addTranslation(-x - 0.5F, -y, -z - 0.5F);
 
 		return false;
